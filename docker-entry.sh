fd8ceaf3f958b156411fcdf6a3f834c2e1a12dc9
František Dvořák <valtri@civ.zcu.cz>
2016-03-29 23:44:54 +0200

    Manage also /etc/hosts (workaround for serious docker bugs with /etc/hosts).

--- docker-entry.sh
+++ docker-entry.sh
@@ -4,6 +4,10 @@ if [ -d /docker/ssh ]; then
   cp -vp /docker/ssh/* /etc/ssh/
 fi
 
+if [ -f /docker/hosts ]; then
+  cp -vp /docker/hosts /etc/
+fi
+
 if [ -f /docker/root/.ssh/authorized_keys ]; then
   mkdir -v /root/.ssh/ 2>/dev/null || :
   cp -vp /docker/root/.ssh/authorized_keys /root/.ssh/
