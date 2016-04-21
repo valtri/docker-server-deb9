4c849df12262b90eb71f492240d6eae85960d96c
František Dvořák <valtri@civ.zcu.cz>
2016-04-21 14:17:52 +0200

    Manage also /etc/resolv.conf.

--- docker-entry.sh
+++ docker-entry.sh
@@ -8,6 +8,10 @@ if [ -f /docker/hosts ]; then
   cp -vp /docker/hosts /etc/
 fi
 
+if [ -f /docker/resolv.conf ]; then
+  cp -vp /docker/resolv.conf /etc/
+fi
+
 if [ -f /docker/root/.ssh/authorized_keys ]; then
   mkdir -v /root/.ssh/ 2>/dev/null || :
   cp -vp /docker/root/.ssh/authorized_keys /root/.ssh/
